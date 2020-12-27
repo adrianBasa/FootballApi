3a7ec8fe0d1e54c63212e933ae0fdbbfeb4efd16
adrianBasa <adryangolubey@yahoo.com>
2020-12-27 13:31:15 +0200

    Update matchTitle.blade.php

--- resources/views/matchTitle.blade.php
+++ resources/views/matchTitle.blade.php
@@ -1,12 +1,12 @@
 <head>@include('includes.header')
 </head>
-<meta property="og:image" content={{$data['thumbnail']}}>  
-<meta property="og:title" content={{$data['title']}}>  
+<meta property="og:image" content={{$data['thumbnail']}}/>  
+<meta property="og:title" content={{$data['title']}}/>  
 <meta property="og:description" content="VideoHighLight."/>  
 <meta name="twitter:card" content="summary" />
-<meta name="twitter:image" content={{$data['thumbnail']}}>
+<meta name="twitter:image" content={{$data['thumbnail']}}/>
 <meta name="twitter:title" content={{$data['title']}}/>
-<meta name="twitter:description" content="VideoHighLight">
+<meta name="twitter:description" content="VideoHighLight"/>
   <title>{{$data['title']}}</title>
 
  <body>
